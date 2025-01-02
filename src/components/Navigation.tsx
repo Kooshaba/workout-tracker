bd5b43977ba39029c67b71076f6e7e265391a39a
Andy Cernera <kooshaba@gmail.com>
2025-01-02 17:11:59 +0900

    increase bottom nav padding

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -14,7 +14,7 @@ export function Navigation() {
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 overflow-x-auto">
       <div className="min-w-full px-4">
-        <div className="flex whitespace-nowrap">
+        <div className="flex whitespace-nowrap pb-4">
           {navItems.map((item) => (
             <Link
               key={item.path}
